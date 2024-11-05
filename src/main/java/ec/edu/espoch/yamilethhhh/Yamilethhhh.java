90f8f02d2c55b190fd85a2c05b2247b62aa783d0
andrea-yamileth <andreachaquinga2005@gmail.com>
2024-11-05 12:16:43 -0500

    yam

---
+++ src/main/java/ec/edu/espoch/yamilethhhh/Yamilethhhh.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.yamilethhhh;
+
+/**
+ *
+ * @author core i3
+ */
+public class Yamilethhhh {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
